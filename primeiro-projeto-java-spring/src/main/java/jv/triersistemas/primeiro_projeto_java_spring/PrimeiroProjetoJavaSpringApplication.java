f47193c92e83fa2412f12e2ce7757e730052fa7b
laus.vandresen <laus.vandresen@triersistemas.com.br>
2024-08-15 20:10:49 -0300

    Primeiro commit

---
+++ primeiro-projeto-java-spring/src/main/java/jv/triersistemas/primeiro_projeto_java_spring/PrimeiroProjetoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.primeiro_projeto_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ primeiro-projeto-java-spring/src/test/java/jv/triersistemas/primeiro_projeto_java_spring/PrimeiroProjetoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.primeiro_projeto_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
